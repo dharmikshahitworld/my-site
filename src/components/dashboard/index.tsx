b7b610b3d3ae59c3ef5001d91f28066ef4b27c5c
Dharmik Shah <59285364+dharmikshahitworld@users.noreply.github.com>
2023-04-25 19:41:21 +0530

    Update index.tsx

--- src/components/dashboard/index.tsx
+++ src/components/dashboard/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Dashboard: React.FC<object> = () => {
   return (
     <div className='border d-flex justify-content-around'>
-      <h1>Dashboard</h1>
+      <h1>Hello Piyu...</h1>
     </div>
   )
 }
